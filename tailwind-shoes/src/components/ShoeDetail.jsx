097c5cadf8a9eb612da98b9d846d91f60448791d
Mehr-code <papa.molla8@gmail.com>
2024-10-10 10:27:32 +0330

    commit

--- tailwind-shoes/src/components/ShoeDetail.jsx
+++ tailwind-shoes/src/components/ShoeDetail.jsx
@@ -33,7 +33,7 @@ export function ShoeDatail() {
 
         {/* Button */}
         <div className="space-x-10">
-          <button className="h-14 w-44 bg-black text-white hover:bg-gray-900 active:bg-gray-700">
+          <button className="btn-press-anim h-14 w-44 bg-black text-white hover:bg-gray-900 active:bg-gray-700 ">
             Add to bag
           </button>
           {/* Link */}
