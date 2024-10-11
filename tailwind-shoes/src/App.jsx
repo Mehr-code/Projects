31d8639b7565a3ea242fe9d0763121eb0d5f5232
Mehr-code <papa.molla8@gmail.com>
2024-10-11 22:44:28 +0330

    commit

--- tailwind-shoes/src/App.jsx
+++ tailwind-shoes/src/App.jsx
@@ -1,11 +1,14 @@
+import { Card } from "./components/Card";
 import { Nav } from "./components/Nav";
 import { ShoeDatail } from "./components/ShoeDetail";
+import { SHOES_LIST } from "./constant";
 
 export function App() {
   return (
     <div className="p-2 xl:px-24 animate-fadeIn">
-      <Nav />
-      <ShoeDatail />
+      {/* <Nav />
+      <ShoeDatail /> */}
+      <Card item={SHOES_LIST[0]} />
     </div>
   );
 }
