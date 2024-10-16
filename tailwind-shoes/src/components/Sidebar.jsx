3cb59a743456554324f9030499538ed09c39f4cc
Mehr-code <papa.molla8@gmail.com>
2024-10-16 22:32:09 +0330

    commit

--- tailwind-shoes/src/components/Sidebar.jsx
+++ tailwind-shoes/src/components/Sidebar.jsx
@@ -1,16 +1,22 @@
 export function Sidebar({ Children, isOpen, onClickClose }) {
   return (
-    <div
-      className={`z-50 left-0 fixed top-0 w-full h-full bg-white transform transition duration-300 ${isOpen ? "translate-x-0" : "translate-x-full"}`}
-    >
-      <button
-        onClick={onClickClose}
-        className="absolute right-4 top-4 p-2 text-black font-bold"
+    <div>
+      <div
+        className={`p-5 z-50 right-0 fixed top-0 w-full h-full md:w-[50%] lg:w-[35%] bg-white transform transition duration-300 shadow-lg ${isOpen ? "translate-x-0" : "translate-x-full"}`}
       >
-        X
-      </button>
-      {Children}
-      <div></div>
+        <button
+          onClick={onClickClose}
+          className="absolute right-4 top-4 p-2 text-black font-bold"
+        >
+          X
+        </button>
+        {Children}
+      </div>
+
+      {/* Overlay */}
+      {isOpen && (
+        <div className="fixed left-0 top-0 z-20 h-full w-full bg-black opacity-50"></div>
+      )}
     </div>
   );
 }
